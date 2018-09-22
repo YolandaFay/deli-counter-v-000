62397da42deee041de0929178beada535b30acda
Yolanda Fay <yolandafay@gmail.com>
2018-09-22 18:44:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,6 +8,9 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, new_customer)
+  katz_deli << new_customer
+  line_length = katz_deli.length 
+  puts "Welcome, #{new_customer}. You are number #{line_length} in line."
 end
 
 def now_serving()
