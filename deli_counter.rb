8d06ece5e58e800e8908d4c2eaa73db95c332708
Yolanda Fay <yolandafay@gmail.com>
2018-09-22 18:39:56 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,8 @@
-def line()
-  line_length = katz_deli.length 
+def line(katz_deli)
+  if katz_deli.empty
+    return "The line is currently empty."
+  else 
+    line_length = katz_deli.length 
 end
 
 def take_a_number(katz_deli, new_customer)
